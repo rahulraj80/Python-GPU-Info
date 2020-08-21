d1e21ef1cb291e6c52eb90f7b19dc6a390a809d7
rahulraj80 <rahulraj80@users.noreply.github.com>
2020-08-21 21:36:03 +0530

    Update CUDAInfo.py
    Convert from Print to Return

--- CUDAInfo.py
+++ CUDAInfo.py
@@ -1,19 +1,23 @@
 from __future__ import print_function
-__author__      = "Zhi-Qiang Zhou"
-__copyright__   = "Copyright 2018"
+__original_author__      = "Zhi-Qiang Zhou"
+__original_copyright__   = "Copyright 2018"
+__author__      = "Rahul Raj"
+__copyright__   = "Copyright 2020"
 
 import pycuda.autoinit
 import pycuda.driver as cuda
 
 def cudaInfo():
-    """Print CUDA information.
+    """Return CUDA information.
     """
     deviceCount = cuda.Device.count()
-    print('There are {:d} CUDA device(s) detected:\n'.format(deviceCount))
-    print(80 * '-')
+    output = {}
+    output{'CUDA device(s) detected':'{:d}'.format(deviceCount)}
     for device_id in range(deviceCount):
-        printDeviceInfo(device_id)
-        print(80 * '-')
+        device_info_dict = getDeviceInfo(device_id)
+        for key_k in device_info_dict:
+            output{key_k:device_info_dict[key_k]}
+    return output
 
 def convertSMVer2Cores(major, minor):
     """GPU Architecture definitions.
@@ -45,8 +49,8 @@ def convertSMVer2Cores(major, minor):
     elif sm == '72': return 64  # Volta Generation (SM 7.2) GV10B class
     elif sm == '75': return 64  # Turing Generation (SM 7.5) TU10x class
 
-def printDeviceInfo(device_id, prefix=''):
-    """Print CUDA device information.
+def getDeviceInfo(device_id, prefix=''):
+    """Return CUDA device information.
 
     Args:
         device_id (int): Device ID
@@ -64,90 +68,90 @@ def printDeviceInfo(device_id, prefix=''):
 
     runtimeVersion = pycuda.driver.get_version()
     driverVersion = pycuda.driver.get_driver_version()
-
-    print(prefix + 'Device {:d}:'.format(device_id), device.name())
-    print(prefix + '  CUDA Driver Version / Runtime Version:        ', 
+    output = {}
+    output{'Device {:d}'.format(device_id): device.name()}
+    output{'CUDA Driver Version / Runtime Version': 
         '{:d}.{:d} / {:d}.{:d}'.format(
             driverVersion // 1000, (driverVersion % 100) // 10,
-            runtimeVersion[0], runtimeVersion[1]))
-    print(prefix + '  CUDA Capability Major/Minor version number:   ', 
-        str(major) + '.' + str(minor))
-    print(prefix + '  Total amount of global memory:                ', 
-        device.total_memory() // 1024**2, 'MBytes')
-    print(prefix + '  ({:2d}) Multiprocessors x ({:3d}) CUDA Cores/MP:   '.format(
-        attrs['MULTIPROCESSOR_COUNT'], cores), 
-        attrs['MULTIPROCESSOR_COUNT'] * cores, 'CUDA Cores')
-    print(prefix + '  GPU Clock rate:                               ', 
-        attrs['CLOCK_RATE'] / 1e3, 'MHz')
-    print(prefix + '  Memory Clock rate:                            ', 
-        attrs['MEMORY_CLOCK_RATE'] / 1e3, 'MHz')
-    print(prefix + '  Memory Bus Width:                             ', 
-        '{:d}-bit'.format(attrs['GLOBAL_MEMORY_BUS_WIDTH']))
-    print(prefix + '  L2 Cache Size:                                ', 
-        attrs['L2_CACHE_SIZE'] / 1024**2, 'MBytes')
-    print(prefix + '  Max Texture Dimension Size (x,y,z):           ', 
-        '1D = ({:d})'.format(attrs['MAXIMUM_TEXTURE1D_WIDTH']))
-    print(prefix + '                                                ', 
+            runtimeVersion[0], runtimeVersion[1])}
+    output{'CUDA Capability Major/Minor version number': 
+        str(major) + '.' + str(minor)}
+    output{'Total amount of global memory': 
+        '{} MBytes'.format(device.total_memory() // 1024**2)}
+    output{'({:2d}) Multiprocessors x ({:3d}) CUDA Cores/MP'.format(
+        attrs['MULTIPROCESSOR_COUNT'], cores): 
+        '{} CUDA Cores'.format(attrs['MULTIPROCESSOR_COUNT'] * cores)}
+    output{'GPU Clock rate':                               ', 
+        '{} MHz'.format(attrs['CLOCK_RATE'] / 1e3)}
+    output{'Memory Clock rate': 
+        '{} MHz'.format(attrs['MEMORY_CLOCK_RATE'] / 1e3)}
+    output{'Memory Bus Width': 
+        '{:d}-bit'.format(attrs['GLOBAL_MEMORY_BUS_WIDTH'])}
+    output{'L2 Cache Size':
+        '{} MBytes'.format(attrs['L2_CACHE_SIZE'] / 1024**2)}
+    output{'Max Texture Dimension Size (x)':
+        '1D = ({:d})'.format(attrs['MAXIMUM_TEXTURE1D_WIDTH'])}
+    output{'Max Texture Dimension Size (x,y)':
         '2D = ({:d}, {:d})'.format(attrs['MAXIMUM_TEXTURE2D_WIDTH'], 
-                                   attrs['MAXIMUM_TEXTURE2D_HEIGHT']))
-    print(prefix + '                                                ', 
+                                   attrs['MAXIMUM_TEXTURE2D_HEIGHT'])}
+    output{'Max Texture Dimension Size (x,y,z)':
         '3D = ({:d}, {:d}, {:d})'.format(attrs['MAXIMUM_TEXTURE3D_WIDTH'], 
                                          attrs['MAXIMUM_TEXTURE3D_HEIGHT'], 
-                                         attrs['MAXIMUM_TEXTURE3D_DEPTH']))
-    print(prefix + '  Max Layered Texture Size (dim) x layers:      ', 
+                                         attrs['MAXIMUM_TEXTURE3D_DEPTH'])}
+    output{'Max Layered Texture Size (1D) x layers':
         '1D = ({:d}) x {:d}'.format(
             attrs['MAXIMUM_TEXTURE1D_LAYERED_WIDTH'], 
-            attrs['MAXIMUM_TEXTURE1D_LAYERED_LAYERS']))
-    print(prefix + '                                                ', 
+            attrs['MAXIMUM_TEXTURE1D_LAYERED_LAYERS'])}
+    output{'Max Layered Texture Size (2D) x layers':
         '2D = ({:d}, {:d}) x {:d}'.format(
             attrs['MAXIMUM_SURFACE2D_LAYERED_WIDTH'], 
             attrs['MAXIMUM_SURFACE2D_LAYERED_HEIGHT'], 
-            attrs['MAXIMUM_SURFACE2D_LAYERED_LAYERS']))
-    print(prefix + '  Total amount of constant memory:              ', 
-        attrs['TOTAL_CONSTANT_MEMORY'], 'Bytes')
-    print(prefix + '  Total amount of shared memory per block:      ', 
-        attrs['MAX_SHARED_MEMORY_PER_BLOCK'], 'Bytes')
-    print(prefix + '  Total number of registers available per block:', 
-        attrs['MAX_REGISTERS_PER_BLOCK'])
-    print(prefix + '  Warp size:                                    ', 
-        attrs['WARP_SIZE'])
-    print(prefix + '  Maximum number of threads per multiprocessor: ', 
-        attrs['MAX_THREADS_PER_MULTIPROCESSOR'])
-    print(prefix + '  Maximum number of threads per block:          ', 
-        attrs['MAX_THREADS_PER_BLOCK'])
-    print(prefix + '  Maximum sizes of each dimension of a block:   ', 
+            attrs['MAXIMUM_SURFACE2D_LAYERED_LAYERS'])}
+    output{'Total amount of constant memory':
+        '{} Bytes'.format(attrs['TOTAL_CONSTANT_MEMORY'])}
+    output{'Total amount of shared memory per block':
+        '{} Bytes'.format(attrs['MAX_SHARED_MEMORY_PER_BLOCK'])}
+    output{'Total number of registers available per block': 
+        attrs['MAX_REGISTERS_PER_BLOCK']}
+    output{'Warp size':
+        attrs['WARP_SIZE']}
+    output{'Maximum number of threads per multiprocessor': 
+        attrs['MAX_THREADS_PER_MULTIPROCESSOR']}
+    output{'Maximum number of threads per block':
+        attrs['MAX_THREADS_PER_BLOCK']}
+    output{'Maximum sizes of each dimension of a block':
         '{:d} x {:d} x {:d}'.format(attrs['MAX_BLOCK_DIM_X'], 
                                     attrs['MAX_BLOCK_DIM_Y'], 
-                                    attrs['MAX_BLOCK_DIM_Z']))
-    print(prefix + '  Maximum sizes of each dimension of a grid:    ', 
+                                    attrs['MAX_BLOCK_DIM_Z'])}
+    output{'Maximum sizes of each dimension of a grid':
         '{:d} x {:d} x {:d}'.format(attrs['MAX_GRID_DIM_X'], 
                                     attrs['MAX_GRID_DIM_Y'], 
-                                    attrs['MAX_GRID_DIM_Z']))
-    print(prefix + '  Maximum memory pitch:                         ', 
-        attrs['MAX_PITCH'], 'Bytes')
-    print(prefix + '  Texture alignment:                            ', 
-        attrs['TEXTURE_ALIGNMENT'], 'Bytes')
-    print(prefix + '  Concurrent copy and kernel execution:         ', 
+                                    attrs['MAX_GRID_DIM_Z'])}
+    output{'Maximum memory pitch':
+        'Bytes'.format(attrs['MAX_PITCH'])}
+    output{'Texture alignment':
+        'Bytes'.format(attrs['TEXTURE_ALIGNMENT'])}
+    output{'Concurrent copy and kernel execution':
         '{} with {:d} copy engine(s)'.format(
             bool(attrs['CONCURRENT_KERNELS']), 
-            attrs['ASYNC_ENGINE_COUNT']))
-    print(prefix + '  Run time limit on kernels:                    ', 
-        bool(attrs['KERNEL_EXEC_TIMEOUT']))
-    print(prefix + '  Integrated GPU sharing Host Memory:           ', 
-        bool(attrs['INTEGRATED']))
-    print(prefix + '  Support host page-locked memory mapping:      ', 
-        bool(attrs['CAN_MAP_HOST_MEMORY']))
-    print(prefix + '  Alignment requirement for Surfaces:           ', 
-        bool(attrs['SURFACE_ALIGNMENT']))
-    print(prefix + '  Device has ECC support:                       ', 
-        bool(attrs['ECC_ENABLED']))
-    print(prefix + '  Device supports Unified Addressing (UVA):     ', 
-        bool(attrs['UNIFIED_ADDRESSING']))
-    print(prefix + '  Device PCI Bus ID / PCI location ID:          ', 
-        attrs['PCI_BUS_ID'], '/', attrs['PCI_DEVICE_ID'])
-    print(prefix + '  Compute Mode:                                 ', 
-        attrs['COMPUTE_MODE'])
+            attrs['ASYNC_ENGINE_COUNT'])}
+    output{'Run time limit on kernels':
+        bool(attrs['KERNEL_EXEC_TIMEOUT'])}
+    output{'Integrated GPU sharing Host Memory':
+        bool(attrs['INTEGRATED'])}
+    output{'Support host page-locked memory mapping':
+        bool(attrs['CAN_MAP_HOST_MEMORY'])}
+    output{'Alignment requirement for Surfaces':
+        bool(attrs['SURFACE_ALIGNMENT'])}
+    output{'Device has ECC support':
+        bool(attrs['ECC_ENABLED'])}
+    output{'Device supports Unified Addressing (UVA)':
+        bool(attrs['UNIFIED_ADDRESSING'])}
+    output{'Device PCI Bus ID / PCI location ID':
+        '{} / {}'.format(attrs['PCI_BUS_ID'], attrs['PCI_DEVICE_ID'])}
+    output{'Compute Mode':
+        attrs['COMPUTE_MODE']}
+    return output
 
 if __name__ == '__main__':
-
-    cudaInfo()
+    print(cudaInfo())
