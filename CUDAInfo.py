b7eea39279917b81324d8ecc77f09a1664120aa4
rahulraj80 <rahulraj80@users.noreply.github.com>
2020-08-21 23:10:54 +0530

    Update CUDAInfo.py

--- CUDAInfo.py
+++ CUDAInfo.py
@@ -74,83 +74,53 @@ def getDeviceInfo(device_id, prefix=''):
         '{:d}.{:d} / {:d}.{:d}'.format(
             driverVersion // 1000, (driverVersion % 100) // 10,
             runtimeVersion[0], runtimeVersion[1])
-    output['CUDA Capability Major/Minor version number']=
-        str(major) + '.' + str(minor)
-    output['Total amount of global memory']=
-        '{} MBytes'.format(device.total_memory() // 1024**2)
+    output['CUDA Capability Major/Minor version number']=        str(major) + '.' + str(minor)
+    output['Total amount of global memory']=        '{} MBytes'.format(device.total_memory() // 1024**2)
     output['({:2d}) Multiprocessors x ({:3d}) CUDA Cores/MP'.format(
-        attrs['MULTIPROCESSOR_COUNT'], cores)]=
-        '{} CUDA Cores'.format(attrs['MULTIPROCESSOR_COUNT'] * cores)
-    output['GPU Clock rate']=
-        '{} MHz'.format(attrs['CLOCK_RATE'] / 1e3)
-    output['Memory Clock rate']=
-        '{} MHz'.format(attrs['MEMORY_CLOCK_RATE'] / 1e3)
-    output['Memory Bus Width']=
-        '{:d}-bit'.format(attrs['GLOBAL_MEMORY_BUS_WIDTH'])
-    output['L2 Cache Size']=
-        '{} MBytes'.format(attrs['L2_CACHE_SIZE'] / 1024**2)
-    output['Max Texture Dimension Size (x)']=
-        '1D = ({:d})'.format(attrs['MAXIMUM_TEXTURE1D_WIDTH'])
-    output['Max Texture Dimension Size (x,y)']=
-        '2D = ({:d}, {:d})'.format(attrs['MAXIMUM_TEXTURE2D_WIDTH'], 
+        attrs['MULTIPROCESSOR_COUNT'], cores)]=        '{} CUDA Cores'.format(attrs['MULTIPROCESSOR_COUNT'] * cores)
+    output['GPU Clock rate']=        '{} MHz'.format(attrs['CLOCK_RATE'] / 1e3)
+    output['Memory Clock rate']=        '{} MHz'.format(attrs['MEMORY_CLOCK_RATE'] / 1e3)
+    output['Memory Bus Width']=        '{:d}-bit'.format(attrs['GLOBAL_MEMORY_BUS_WIDTH'])
+    output['L2 Cache Size']=        '{} MBytes'.format(attrs['L2_CACHE_SIZE'] / 1024**2)
+    output['Max Texture Dimension Size (x)']=        '1D = ({:d})'.format(attrs['MAXIMUM_TEXTURE1D_WIDTH'])
+    output['Max Texture Dimension Size (x,y)']=        '2D = ({:d}, {:d})'.format(attrs['MAXIMUM_TEXTURE2D_WIDTH'], 
                                    attrs['MAXIMUM_TEXTURE2D_HEIGHT'])
-    output['Max Texture Dimension Size (x,y,z)']=
-        '3D = ({:d}, {:d}, {:d})'.format(attrs['MAXIMUM_TEXTURE3D_WIDTH'], 
+    output['Max Texture Dimension Size (x,y,z)']=        '3D = ({:d}, {:d}, {:d})'.format(attrs['MAXIMUM_TEXTURE3D_WIDTH'], 
                                          attrs['MAXIMUM_TEXTURE3D_HEIGHT'], 
                                          attrs['MAXIMUM_TEXTURE3D_DEPTH'])
-    output['Max Layered Texture Size (1D) x layers']=
-        '1D = ({:d}) x {:d}'.format(
+    output['Max Layered Texture Size (1D) x layers']=        '1D = ({:d}) x {:d}'.format(
             attrs['MAXIMUM_TEXTURE1D_LAYERED_WIDTH'], 
             attrs['MAXIMUM_TEXTURE1D_LAYERED_LAYERS'])
-    output['Max Layered Texture Size (2D) x layers']=
-        '2D = ({:d}, {:d}) x {:d}'.format(
+    output['Max Layered Texture Size (2D) x layers']=        '2D = ({:d}, {:d}) x {:d}'.format(
             attrs['MAXIMUM_SURFACE2D_LAYERED_WIDTH'], 
             attrs['MAXIMUM_SURFACE2D_LAYERED_HEIGHT'], 
             attrs['MAXIMUM_SURFACE2D_LAYERED_LAYERS'])
-    output['Total amount of constant memory']=
-        '{} Bytes'.format(attrs['TOTAL_CONSTANT_MEMORY'])}
+    output['Total amount of constant memory']=        '{} Bytes'.format(attrs['TOTAL_CONSTANT_MEMORY'])}
     output['Total amount of shared memory per block':
         '{} Bytes'.format(attrs['MAX_SHARED_MEMORY_PER_BLOCK'])
-    output['Total number of registers available per block']=
-        attrs['MAX_REGISTERS_PER_BLOCK']
-    output['Warp size']=
-        attrs['WARP_SIZE']
-    output['Maximum number of threads per multiprocessor']=
-        attrs['MAX_THREADS_PER_MULTIPROCESSOR']
-    output['Maximum number of threads per block']=
-        attrs['MAX_THREADS_PER_BLOCK']
-    output['Maximum sizes of each dimension of a block']=
-        '{:d} x {:d} x {:d}'.format(attrs['MAX_BLOCK_DIM_X'], 
+    output['Total number of registers available per block']=        attrs['MAX_REGISTERS_PER_BLOCK']
+    output['Warp size']=        attrs['WARP_SIZE']
+    output['Maximum number of threads per multiprocessor']=        attrs['MAX_THREADS_PER_MULTIPROCESSOR']
+    output['Maximum number of threads per block']=        attrs['MAX_THREADS_PER_BLOCK']
+    output['Maximum sizes of each dimension of a block']=        '{:d} x {:d} x {:d}'.format(attrs['MAX_BLOCK_DIM_X'], 
                                     attrs['MAX_BLOCK_DIM_Y'], 
                                     attrs['MAX_BLOCK_DIM_Z'])
-    output['Maximum sizes of each dimension of a grid']=
-        '{:d} x {:d} x {:d}'.format(attrs['MAX_GRID_DIM_X'], 
+    output['Maximum sizes of each dimension of a grid']=        '{:d} x {:d} x {:d}'.format(attrs['MAX_GRID_DIM_X'], 
                                     attrs['MAX_GRID_DIM_Y'], 
                                     attrs['MAX_GRID_DIM_Z'])
-    output['Maximum memory pitch']=
-        'Bytes'.format(attrs['MAX_PITCH'])
-    output['Texture alignment']=
-        'Bytes'.format(attrs['TEXTURE_ALIGNMENT'])
-    output['Concurrent copy and kernel execution']=
-        '{} with {:d} copy engine(s)'.format(
+    output['Maximum memory pitch']=        'Bytes'.format(attrs['MAX_PITCH'])
+    output['Texture alignment']=        'Bytes'.format(attrs['TEXTURE_ALIGNMENT'])
+    output['Concurrent copy and kernel execution']=        '{} with {:d} copy engine(s)'.format(
             bool(attrs['CONCURRENT_KERNELS']), 
             attrs['ASYNC_ENGINE_COUNT'])
-    output['Run time limit on kernels']=
-        bool(attrs['KERNEL_EXEC_TIMEOUT'])
-    output['Integrated GPU sharing Host Memory']=
-        bool(attrs['INTEGRATED'])
-    output['Support host page-locked memory mapping']=
-        bool(attrs['CAN_MAP_HOST_MEMORY'])
-    output['Alignment requirement for Surfaces']=
-        bool(attrs['SURFACE_ALIGNMENT'])
-    output['Device has ECC support']=
-        bool(attrs['ECC_ENABLED'])
-    output['Device supports Unified Addressing (UVA)']=
-        bool(attrs['UNIFIED_ADDRESSING'])
-    output['Device PCI Bus ID / PCI location ID']=
-        '{} / {}'.format(attrs['PCI_BUS_ID'], attrs['PCI_DEVICE_ID'])
-    output['Compute Mode']=
-        attrs['COMPUTE_MODE']
+    output['Run time limit on kernels']=        bool(attrs['KERNEL_EXEC_TIMEOUT'])
+    output['Integrated GPU sharing Host Memory']=        bool(attrs['INTEGRATED'])
+    output['Support host page-locked memory mapping']=        bool(attrs['CAN_MAP_HOST_MEMORY'])
+    output['Alignment requirement for Surfaces']=        bool(attrs['SURFACE_ALIGNMENT'])
+    output['Device has ECC support']=        bool(attrs['ECC_ENABLED'])
+    output['Device supports Unified Addressing (UVA)']=        bool(attrs['UNIFIED_ADDRESSING'])
+    output['Device PCI Bus ID / PCI location ID']=        '{} / {}'.format(attrs['PCI_BUS_ID'], attrs['PCI_DEVICE_ID'])
+    output['Compute Mode']=        attrs['COMPUTE_MODE']
     return output
 
 if __name__ == '__main__':
